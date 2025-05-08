7c7af938789a0cdf5651b9c15cd8d458d8fd1238
development <development@duizendstra.com>
2025-05-08 17:38:16 +0000

    chore: Update AppVersion to v0.0.6 and add tag

--- cmd/root.go
+++ cmd/root.go
@@ -208,7 +208,7 @@ var (
 func init() {
 	// Set the application version. This can be overridden by ldflags during build.
 	if AppVersion == "" {
-		AppVersion = "v0.0.5" // Default version if not set by build flags
+		AppVersion = "v0.0.6" // Default version if not set by build flags
 	}
 
 	// Define persistent flags available to all commands.
