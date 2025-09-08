3a582c194b0c09b3c1b12a56eca5233bdc73d5c5
Jasper Duizendstra <jasper@duizendstra.com>
2025-09-08 14:50:23 +0200

    fix (#71)
    Co-authored-by: contextvibes <contextvibes@duizendstra.com>

--- .idx/contextvibes.nix
+++ .idx/contextvibes.nix
@@ -8,10 +8,10 @@ pkgs.buildGoModule {
   src = pkgs.fetchFromGitHub {
     owner = "contextvibes";
     repo = "cli";
-    rev = "92503c4e1debd8cb36248e26cc9341cb5831bb91";
-    hash = "sha256-Zos8r3IOz4lzN5Yox6TNtFus8pm955IxdHsbZCPos4g=";
+    rev = "8c9fe4f307fea4234bdd238913153753a5a7a688";
+    hash = "sha256-KXmq00huV+e9LIzLt5xhKQF/HfCCg1GrY1BYkSv4MJo=";
   };
 
   vendorHash = null;
-  subPackages = [ "cmd/cv" ];
+  subPackages = [ "cmd/contextvibes" ];
 }
