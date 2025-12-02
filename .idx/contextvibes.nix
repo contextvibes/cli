24b4a7d574c711c6946cb8207956251614cce0ee
Jasper Duizendstra <jasper@duizendstra.com>
2025-12-02 08:08:07 +0000

    chore(nix): update to v0.4.1-rc3 (pure build)

--- .idx/contextvibes.nix
+++ .idx/contextvibes.nix
@@ -1,24 +1,17 @@
 # .idx/contextvibes.nix
-# Downloads a pre-compiled binary from a GitHub Release.
 { pkgs }:
 
 pkgs.stdenv.mkDerivation {
   pname = "contextvibes";
-  version = "0.4.0";
+  version = "v0.4.1-rc3";
 
-  # Fetch the pre-built binary from the GitHub Release.
   src = pkgs.fetchurl {
-    # URL for the release asset.
-    url = "https://github.com/contextvibes/cli/releases/download/v0.4.0/contextvibes";
-    # SHA256 hash of the downloaded file.
-    sha256 = "sha256:3a6a5196c90a5e2dc910d1c819e450246f47127855b79a11869f5c6d3274ca6f";
+    url = "https://github.com/contextvibes/cli/releases/download/v0.4.1-rc3/contextvibes";
+    sha256 = "1aa09c34c750056e78a69e84f6d7c38a0c6220e3dc46af460256f9a38978ff8a";
   };
 
   dontUnpack = true;
 
-  # Install the binary into the output directory.
-  # $src refers to the downloaded file.
-  # $out is the destination path in the Nix store.
   installPhase = ''
     mkdir -p $out/bin
     install -m 755 -D $src $out/bin/contextvibes
